87dc146437129e6c31c708aa23ba76e735f2ba6c
MateoLa <mateo.laino@gmail.com>
2020-02-03 15:51:25 -0300

    first

---
+++ lib/spree_dyn_related_products.rb
@@ -0,0 +1,5 @@
+require "spree_dyn_related_products/railtie"
+
+module SpreeDynRelatedProducts
+  # Your code goes here...
+end
---
+++ lib/tasks/spree_dyn_related_products_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :spree_dyn_related_products do
+#   # Task goes here
+# end
