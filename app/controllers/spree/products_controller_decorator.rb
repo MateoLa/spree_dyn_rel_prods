91bfe1924014ee03499c18d868c8b66924568053
MateoLa <mateo.laino@gmail.com>
2020-02-05 13:35:51 -0300

    first

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,20 +1,16 @@
 module Spree::ProductsControllerDecorator
-#  def self.prepended(base)
-#    base.before_action :load_product, only: :related
-#  end
 
   def related
     if @product.respond_to?(:has_related_products?)
       super
     else
-      @_related_products ||= []
+      @related_products ||= []
       if @product.taxons.present?
-        max_deep = @product.taxons.map { |e| e[:depth] }.max
-        taxons = @product.taxons.find_by(depth: max_deep)
+        max_deep = @product.taxons.maximum(:depth)
+        taxons = @product.taxons.where(depth: max_deep)
 
         taxons.each do |taxon|
-byebug
-          @_related_products << taxon.products.
+          @related_products |= taxon.products.where.not(id: @product.id).
                                 includes(
                                   :tax_category,
                                   master: [
@@ -33,18 +29,6 @@ byebug
     end
 
   end
-
-#    def load_product
-#      @products = if try_spree_current_user.try(:has_spree_role?, 'admin')
-#                    Product.with_deleted
-#                  else
-#                    Product.active(current_currency)
-#                  end
-
-#      @product = @products.includes(:master).
-#                 friendly.
-#                 find(params[:id])
-#    end
 
 end
 
