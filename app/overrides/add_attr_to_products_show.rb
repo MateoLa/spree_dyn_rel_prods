9b231fd37d4f7dec8f2fd47392b020360a43176d
MateoLa <mateo.laino@gmail.com>
2020-02-07 12:12:07 -0300

    first

--- app/overrides/add_attr_to_products_show.rb
+++ app/overrides/add_attr_to_products_show.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'add_attr_to_products_show',
   add_to_attributes: "[data-related-products]",
-  attributes: {:"data-related-products-taxon-id" => "<%= @taxon.id %>"},
+  attributes: {:"data-related-products-taxon-id" => "<%= @taxon.present? ? @taxon.id : '' %>"},
   original: 'b7736d9aecaf892a576ad1155a7fa8256f6266f0'
 )
