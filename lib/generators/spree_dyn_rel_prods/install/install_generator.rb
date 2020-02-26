778e479a45ba73175b9469875b7bd93bc5b634b6
MateoLa <mateo.laino@gmail.com>
2020-02-26 18:39:30 -0300

    first

--- lib/generators/spree_dyn_rel_prods/install/install_generator.rb
+++ lib/generators/spree_dyn_rel_prods/install/install_generator.rb
@@ -2,10 +2,10 @@ module SpreeDynRelProds
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      def add_assets_into_spree_files
-        # "Injecting into File" avoids the need to override layouts
+      def add_assets_into_spree
+        # "Injecting to File" avoids the need to override layouts
         # adding scripts or stylesheets tags lines.
-        inject_into_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require dyn_rel_prods/dyn_rel_prods\n", after: "spree/frontend", verbose: true
+        inject_into_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require dyn_rel_prods/dyn_rel_prods", after: "spree/frontend", verbose: true
       end
 
     end
